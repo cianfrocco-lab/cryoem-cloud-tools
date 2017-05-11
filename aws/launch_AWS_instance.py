005fbcb0456f115a5aa596d70bcc8911eabc9f8f
Michael Cianfrocco <michael.a.cianfrocco@gmail.com>
2017-05-10 21:22:32 -0700

    Added python version checking for fabric command

--- aws/launch_AWS_instance.py
+++ aws/launch_AWS_instance.py
@@ -376,9 +376,12 @@ def module_exists(module_name):
 def exec_remote_cmd(cmd):
     from fabric.operations import run, put
     from fabric.api import hide,settings
-    with hide('output','running','warnings'), settings(warn_only=True):
-        return run(cmd)
-
+    if float(sys.version_info[1])>=7: 
+	with hide('output','running','warnings'), settings(warn_only=True):
+        	return run(cmd)
+    if float(sys.version_info[1])<7:
+	with hide('output','running','warnings') and settings(warn_only=True):
+                return run(cmd)
 #==============================
 def query_yes_no(question, default="no"):
 	valid = {"yes": True, "y": True, "ye": True,"no": False, "n": False}
@@ -404,7 +407,7 @@ def query_yes_no(question, default="no"):
 #==============================
 if __name__ == "__main__":
 
-    availInstances=['t2.micro','t2.nano','t2.small','t2.medium','t2.large','i2.2xlarge','i2.xlarge','m4.large','m4.xlarge','m4.2xlarge','m4.4xlarge','m4.10xlarge','m4.16xlarge','m3.medium','m3.large','m3.xlarge','m3.2xlarge','c4.large','c4.xlarge','c4.2xlarge','c4.4xlarge','c4.8xlarge','c3.large','c3.xlarge','c3.2xlarge','c3.4xlarge','c3.xlarge','r4.16xlarge','r4.4xlarge','r4.8xlarge','r3.large','r3.xlarge','r3.2xlarge','r3.4xlarge','r3.8xlarge','d2.xlarge','d2.2xlarge','d2.8xlarge','p2.xlarge','p2.8xlarge','p2.16xlarge','g2.2xlarge','g2.8xlarge']
+    availInstances=['t2.micro','t2.nano','t2.small','t2.medium','t2.large','i2.2xlarge','i2.xlarge','m4.large','m4.xlarge','m4.2xlarge','m4.4xlarge','m4.10xlarge','m4.16xlarge','m3.medium','m3.large','m3.xlarge','m3.2xlarge','c4.large','c4.xlarge','c4.2xlarge','c4.4xlarge','c4.8xlarge','c3.large','c3.xlarge','c3.2xlarge','c3.4xlarge','c3.xlarge','r4.16xlarge','r4.4xlarge','r4.8xlarge','r3.large','r3.xlarge','r3.2xlarge','r3.4xlarge','r3.8xlarge','x1.32xlarge','d2.xlarge','d2.2xlarge','d2.8xlarge','p2.xlarge','p2.8xlarge','p2.16xlarge','g2.2xlarge','g2.8xlarge']
 
     params=setupParserOptions()
     if params['listInstance'] is True:
