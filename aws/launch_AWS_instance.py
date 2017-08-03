d89fd2c749093f6ea29d5db836f2c2c9534e2ce1
Michael Cianfrocco <michael.a.cianfrocco@gmail.com>
2017-08-02 20:57:22 -0700

    Updated launch with SG issue patch

--- aws/launch_AWS_instance.py
+++ aws/launch_AWS_instance.py
@@ -138,6 +138,9 @@ def launchInstance(params,keyName,keyPath,AMI,AWS_ACCOUNT_ID):
 
     print '\nLaunching AWS instance %s for user %s\n' %(params['instance'],keyName)
 
+    if params['debug'] is True: 
+	print 'version 100'
+
     uname=subprocess.Popen('uname',shell=True, stdout=subprocess.PIPE).stdout.read().strip()
     securityGroupName='sg_%i' %(int(time.time()))
     securityGroupDescript='Automated security group'
@@ -396,7 +399,7 @@ def query_yes_no(question, default="no"):
 #==============================
 if __name__ == "__main__":
 
-    availInstances=['t2.micro','t2.nano','t2.small','t2.medium','t2.large','i2.2xlarge','i2.xlarge','m4.large','m4.xlarge','m4.2xlarge','m4.4xlarge','m4.10xlarge','m4.16xlarge','m3.medium','m3.large','m3.xlarge','m3.2xlarge','c4.large','c4.xlarge','c4.2xlarge','c4.4xlarge','c4.8xlarge','c3.large','c3.xlarge','c3.2xlarge','c3.4xlarge','c3.xlarge','r4.16xlarge','r4.4xlarge','r4.8xlarge','r3.large','r3.xlarge','r3.2xlarge','r3.4xlarge','r3.8xlarge','x1.32xlarge','d2.xlarge','d2.2xlarge','d2.8xlarge','p2.xlarge','p2.8xlarge','p2.16xlarge','g2.2xlarge','g2.8xlarge']
+    availInstances=['t2.micro','t2.nano','t2.small','t2.medium','t2.large','i2.2xlarge','i2.xlarge','m4.large','m4.xlarge','m4.2xlarge','m4.4xlarge','m4.10xlarge','m4.16xlarge','m3.medium','m3.large','m3.xlarge','m3.2xlarge','c4.large','c4.xlarge','c4.2xlarge','c4.4xlarge','c4.8xlarge','c3.large','c3.xlarge','c3.2xlarge','c3.4xlarge','c3.xlarge','r4.16xlarge','r4.4xlarge','r4.8xlarge','r3.large','r3.xlarge','r3.2xlarge','r3.4xlarge','r3.8xlarge','x1.32xlarge','d2.xlarge','d2.2xlarge','d2.8xlarge','p2.xlarge','p2.8xlarge','p2.16xlarge','g3.4xlarge','g3.8xlarge','g3.16xlarge','g2.2xlarge','g2.8xlarge']
 
     params=setupParserOptions()
     if params['listInstance'] is True:
