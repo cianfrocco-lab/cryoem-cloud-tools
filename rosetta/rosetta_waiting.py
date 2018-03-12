24fbb755341924b5bc9b7c8e8621a98e35d8f5a6
mcianfrocco <michael.a.cianfrocco@gmail.com>
2018-03-11 22:05:51 -0400

    Removed debugging print numtot

--- rosetta/rosetta_waiting.py
+++ rosetta/rosetta_waiting.py
@@ -99,7 +99,6 @@ if __name__ == "__main__":
 	                time.sleep(30)
 			currentTime=datetime.datetime.utcnow()
 			numtot=subprocess.Popen('ssh -q -n -f -i %s ubuntu@%s "/bin/ls * | wc -l"'%(keypair,instanceIPlist[counter]),shell=True, stdout=subprocess.PIPE).stdout.read().strip()
-			print numtot
 			if float(numtot) > 25: 
 				numPDB=subprocess.Popen('ssh -q -n -f -i %s ubuntu@%s "/bin/ls S*pdb | wc -l"'%(keypair,instanceIPlist[counter]),shell=True, stdout=subprocess.PIPE).stdout.read().strip()
 	                        subprocess.Popen(cmd,shell=True).wait()
